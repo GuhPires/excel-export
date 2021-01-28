9cebcbbdd1747b7d0002901bb2c25d3e574221ba
Gustavo <gustavo.goncalvespires@gmail.com>
2021-01-28 13:25:58 -0300

    Added helpful comments to the ExportFile Class

--- src/ExportFile.ts
+++ src/ExportFile.ts
@@ -8,7 +8,9 @@ export default class ExportFile extends Workbook {
 		this.fileType = fileType;
 	}
 
+	// Initialization method
 	init(wsName: string, columns: string[]): Iterator<object[]> | any {
+		// Checking if the method has been called for this instance
 		if (this.wb instanceof Workbook) {
 			console.error(
 				'ERROR! Cannot call the "init()" method more than once!\nIf you want to create a new Worksheet, please use "addWs()" method.'
@@ -16,22 +18,28 @@ export default class ExportFile extends Workbook {
 			return null;
 		}
 		this.wb = new Workbook();
+		// Returning the Worksheet, Workbook and the instance of this class
 		return [this.addWs(wsName, columns), this.wb, this];
 	}
 
+	// Creating Workshet for the current Workbook
 	addWs(name: string, columns: string[]): object {
 		const ws = this.wb.addWorksheet(name);
+		// Creating the column headers
 		ws.columns = columns.map(c => ({
 			header: c.toString(),
 			key: c.toString().toLowerCase(),
 		}));
+		// Returning the newly created Worksheet
 		return ws;
 	}
 
+	// Exporting current Workbook in the format specified into the contructor
 	async exportWb(fileName: string): Promise<string> {
 		const finalName = `${fileName}.${this.fileType}`;
-		// Change the 'writeFile' to 'write' in order to write in a stream.
+		// Change the 'writeFile' method to 'write' in order to write in a stream.
 		await this.wb[this.fileType].writeFile(finalName);
+		// Returning the name of the exported file with the extension
 		return finalName;
 	}
 }
